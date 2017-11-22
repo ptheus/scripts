e2ef147d5a130330037d5bd8db956b904b10065d
ptheus <xingzhe_sust@163.com>
2017-11-23 00:00:39 +0800

    提交第一个脚本

---
+++ Repositories_Update.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# 开启外网访问权限，同步远程服务器git仓库到本地服务器
+# 同步完毕后关闭外网访问权限
+
+# vars
+UPDATELOG='update.log'
+ERRORFLAG=0
+# UPREPOS="freeswitch.git opensips-cp.git opensips.git"
+UPREPOS=(freeswitch.git opensips-cp.git opensips.git)
+GWAY_DNS="192.168.2.99"
+
+# log
+echo "$(date '+%Y-%m-%d %H:%M')" >> ${UPDATELOG} 
+
+# modify cfg for connect wlan net
+sudo sed -i "s/GATEWAY=/GATEWAY=${GWAY_DNS}/" /etc/sysconfig/network-scripts/ifcfg-ens192
+sudo sed -i "s/DNS1=/DNS1=${GWAY_DNS}/" /etc/sysconfig/network-scripts/ifcfg-ens192
+{ sudo systemctl restart network } || \
+	{ echo -e "\tConnect net out failed!" >> ${UPDATELOG};\
+	exit 1 }
+
+# sync needed repoitories
+# for REPO in ${UPREPOS}
+for REPO in ${UPREPOS[*]}
+do
+	git --git-dir=${HOME}/repositories/${REPO} remote update
+	if [ $? -ne 0 ]
+	then
+		ERRORFLAG=1
+		echo -e "\t${REPO} update failed!" \
+			>> ${UPDATELOG}
+	fi
+done
+
+# log
+if [ 0 -eq ${ERRORFLAG} ]
+then
+	echo -e "\tAll update is done." >> ${UPDATELOG}
+fi
+
+# modify cfg for machine cut wlan net only for lan use
+sudo sed -i "s/${GWAY_DNS}//g" /etc/sysconfig/network-scripts/ifcfg-ens192
+{ sudo systemctl restart network } || echo -e "\tCut wlan net failed!" >> ${UPDATELOG}
+
+# 特别说明：脚本放到指定位置后添加定时任务
+# 例如：30 1 * * * git /home/git/bin/Repositories_Update.sh
+# 重启定时任务 systemctl restart crond
